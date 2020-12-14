92de0fdc6607d6add7a2cf8fe1ef0b021fdc1451
asaskiap <alannapfeiffer@gmail.com>
2020-12-14 12:42:26 +0100

    made modular

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 const express = require('express');
 
+//for locating partials
+const hbs = require('hbs');
+
+// to be able to use partials in my handlebars templates
+// I need to tell the hbs engine where to look for thiem
+hbs.registerPartials(__dirname + '/views/partials');
+
 const app = express();
 
+// prerequisite to use handlebars templates
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+// set default properties
+app.locals.pageTitle = 'default page';
+
+//make public folder available
 app.use(express.static('public'));
 
 app.get('/about', function(request, response) {
-    response.sendFile(__dirname + '/views/about.html');
+    response.render('about', {
+        pageTitle: 'About',
+        layout: 'layout.hbs'
+    });
 });
 app.get('/works', function(request, response) {
-    response.sendFile(__dirname + '/views/works.html');
+    response.render('works', {
+        pageTitle: 'Works'
+    });
 });
 app.get('/gallery', function(request, response) {
-    response.sendFile(__dirname + '/views/gallery.html');
+    response.render('gallery', {
+        pageTitle: 'Gallery'
+    });
 });
 app.get('*', function(request, response) {
-    response.sendFile(__dirname + '/views/index.html');
+    response.render('home', {
+        pageTitle: 'Home',
+        layout: '/layout'
+    });
 });
 
 app.listen(3000);
